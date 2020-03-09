280a4a93075e1509a983282842505666424da3ce
alfa9317 <josealfredotg@gmail.com>
2020-03-08 21:20:41 -0600

    Updates

---
+++ precache-manifest.4225628ebc0a6711e326672fb3969785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1605c77e75c4a050d528d55eb4e869b1",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "c97ed9dbb335a1abbe42",
+    "url": "/My-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c18c16231221e12405c",
+    "url": "/My-Portfolio/static/js/2.2717102d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/My-Portfolio/static/js/2.2717102d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97ed9dbb335a1abbe42",
+    "url": "/My-Portfolio/static/js/main.0b0cd464.chunk.js"
+  },
+  {
+    "revision": "88f788b78598e8210ad8",
+    "url": "/My-Portfolio/static/js/runtime-main.eabc6d2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
